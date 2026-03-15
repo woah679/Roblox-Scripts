e85cf19f79a7e0aba75ed1d7fca0698e980962a9
woah679 <83089186+woah679@users.noreply.github.com>
2026-03-15 18:10:21 -0500

    Create ExtraVMods.lua

---
+++ ExtraVMods.lua
@@ -0,0 +1,8 @@
+repeat task.wait() until _G.WindUI and _G.Functions
+
+local WindUI = _G.WindUI
+local Window = _G.Window
+local Tabs = _G.Tabs
+
+local Connections = _G.Connections
+local Functions = _G.Functions
